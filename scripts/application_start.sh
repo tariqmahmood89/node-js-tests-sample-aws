6fb9eb77536b03de56501a90ebd9c2f2f26a19d3
tariqmahmood89 <118723567+tariqmahmood89@users.noreply.github.com>
2023-09-15 12:20:40 +0000

    files update

---
+++ scripts/application_start.sh
@@ -0,0 +1,6 @@
+!/bin/bash
+
+# Move to the directory
+cd /home/ec2-user/<app-name>
+
+pm2 reload 0
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+
+# Move to the directory
+cd /home/ec2-user/<app-name>
+
+# Install dependencies
+npm i
+
+# Build the Code
+#npm run build
